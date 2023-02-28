8d9e5887cfd92f9c77e22722de3d3cca4c90d1a4
jmartins9 <joaoantonioredondo1994@gmail.com>
2023-02-28 18:42:47 +0000

    acrescentar id feito

---
+++ TPC3/putIds.py
@@ -0,0 +1,18 @@
+import json
+
+def main():
+    input_file = open("dataset-extra1.json","r", encoding="utf-8")
+    output_file = open("dataset-extra-com-id.json", "w", encoding="utf-8")
+    dataset = json.load(input_file)
+    
+    pessoas = dataset["pessoas"]
+    id = 1
+     
+    for pessoa in pessoas:
+        pessoa["id"] = "p" + str(id)
+        id += 1
+
+    json.dump(dataset, output_file, indent=2, ensure_ascii = False)
+    
+if __name__ == '__main__':
+    main()
