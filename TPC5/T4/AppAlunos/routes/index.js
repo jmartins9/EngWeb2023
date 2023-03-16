dfe108ec0928c8944c3f90ade196533e3982d2b9
jmartins9 <joaoantonioredondo1994@gmail.com>
2023-03-16 15:22:15 +0000

    Página do aluno feita em pug

---
+++ TPC5/T4/AppAlunos/routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var Aluno = require('../controllers/aluno')
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var data = new Date().toISOString().substring(0, 16)
+  Aluno.list()
+    .then(alunos => {
+      res.render('index', { slist: alunos, d: data  });
+    })
+    .catch(erro => {
+      res.render('error', {error: erro, message: "Erro na obtenção na lista de alunos"})
+    })
+
+});
+
+/* GET student page. */
+router.get('/alunos/:idAluno', function(req, res, next) {
+  var data = new Date().toISOString().substring(0, 16)
+  Aluno.getAluno(req.params.idAluno)
+    .then(aluno => {
+      res.render('aluno', { a: aluno, d: data  });
+    })
+    .catch(erro => {
+      res.render('error', {error: erro, message: "Erro na obtenção do registo do aluno"})
+    })
+
+});
+
+
+module.exports = router;
