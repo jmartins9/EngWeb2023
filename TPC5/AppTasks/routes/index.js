571f3370f2524f48e477858b57b240021cbabd03
jmartins9 <joaoantonioredondo1994@gmail.com>
2023-03-18 15:03:18 +0000

    Página inicial feita

--- TPC5/AppTasks/routes/index.js
+++ TPC5/AppTasks/routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var Task = require('../controllers/task')
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  var data = new Date().toISOString().substring(0, 16)
+  Task.list()
+    .then(tasks => {
+      res.render('index', { taskList: tasks, d: data})
+    })
+    .catch(erro => {
+      res.render('error', {error: erro, message: "Erro na obtenção da lista de tarefas."})
+    })
+})
+
+
 
 module.exports = router;
